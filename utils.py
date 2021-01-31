b65dc55aeb008edee3afb89a91e4d49d0c2a175e
June Kwon <herbwood27@gmail.com>
2021-01-31 18:28:34 +0900

    added dataset downloading function

--- utils.py
+++ utils.py
@@ -1,6 +1,25 @@
 import pickle
+import requests
+import tarfile
 
 def unpickle(file : str):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
+
+def download_dataset(url : str = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+             target_path : str = 'cifar-10-python.tar.gz'):
+
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(target_path, 'wb') as f:
+            f.write(response.raw.read())
+
+    if target_path.endswith("tar.gz"):
+
+        tar = tarfile.open(target_path, "r:gz")
+        tar.extractall()
+        tar.close()
+
+if __name__ == "__main__":
+    download_dataset()
