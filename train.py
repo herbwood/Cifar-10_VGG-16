801f7ebdf265e7979fc493bb00c243e8dce57f28
June Kwon <herbwood27@gmail.com>
2021-01-31 22:55:19 +0900

    added checkpoint, learning rate scheduler

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision.transforms as transforms
 import torch.optim as optim
+from torch.optim import lr_scheduler
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from model import VGG
@@ -14,25 +15,36 @@ EPOCHS = 30
 NUM_WORKERS = 2
 
 
-def train_fn(train_loader, model, optimizer, epoch):
+def train_fn(train_loader, model, optimizer, criterion, epoch):
     model.train()
 
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(DEVICE), target.to(DEVICE)
 
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.cross_entropy(output, target)
-        loss.backward()
-        optimizer.step()
+
+        with torch.set_grad_enabled(True):
+            output = model(data)
+            loss = criterion(output, target)
+
+            pred = output.max(1, keepdim=True)[1]
+            correct = pred.eq(target.view_as(pred)).sum().item()
+
+            loss.backward()
+            optimizer.step()
 
         if batch_idx % 100 == 0:
-            print(f"Train Epoch : {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss : {loss.item():.6f}")
+            print(f"Train Epoch : {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]"
+                  f"\tLoss : {loss.item():.6f}"
+                  f"\tAccuracy : {(100. * correct / train_loader.batch_size):.2f}%")
 
+    return loss
 
 def train():
     model = VGG().to(DEVICE)
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+    criterion = nn.CrossEntropyLoss()
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -44,12 +56,24 @@ def train():
                               num_workers=NUM_WORKERS,
                               shuffle=True)
 
+    leastloss = 1e+5
+
     for epoch in range(1, EPOCHS + 1):
         print(f"Epoch [{epoch}/{EPOCHS}]")
-        train_fn(train_loader, model, optimizer, epoch)
+        loss = train_fn(train_loader, model, optimizer, criterion, epoch)
+        scheduler.step()
 
-    torch.save(model.state_dict(), "./model.pt")
+        if loss < leastloss:
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': criterion,
+            }, f'model_epoch_{epoch}_loss_{loss}.pt')
 
+            leastloss = loss
+
+            print(f"saved model with loss : {leastloss:.4f}")
 
 if __name__ == "__main__":
     train()
